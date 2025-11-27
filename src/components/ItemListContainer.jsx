b71a509aaa1e86bfc5b018be7299bd7f44a3f692
Lucía <luciarolon00@gmail.com>
2025-11-27 16:51:44 -0300

    carga de juegos

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,6 +1,11 @@
 import JuegoCard from './JuegoCard.jsx';
 
 const ItemListContainer = ({ juegos }) => {
+
+  if (!juegos || juegos.length === 0) {
+    return <p className="text-center text-yellow-400 text-xl p-8">Cargando juegos...</p>;
+  }
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 p-8'>
       {juegos.map((juego) => (
